0d03946bde43f5e87655e49eaa448da0ff2eef83
Yunir Dautov <dautovyunir93@gmail.com>
2023-03-02 19:54:30 +0500

    Test

--- app/src/main/java/com/example/myshoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/myshoppinglist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.myshoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository) {
-
+    //test a new branches in branches
     fun getShopItem(shopItemId: Int): ShopItem{
         return shopListRepository.getShopItem(shopItemId)
     }
